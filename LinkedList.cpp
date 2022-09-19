e2ede76cad7bc7c9f44c34e500d3c490250e34d4
thayes1 <60246963+thayes1@users.noreply.github.com>
2022-09-18 20:58:08 -0400

    Update LinkedList.cpp

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -32,6 +32,67 @@ void LinkedList::InsertFront(double value) {
     *head = newNode;
 }
 
+//One Method
+public class Node {
+	Node* next; //Takes a next value
+	int data; //Declaring some data
+	public Node(int data) {
+		this.data = data;  //Constructor for making the data
+	}
+
+	public class LinkedList {
+		Node head;
+
+		public void append(int data) {
+			if (head == null) {
+				head = new Node(data);
+				return;
+			}
+		}
+		Node current = head; //Node: current = this ->data
+		while (current.next != null) {
+			current = current.next;
+		}
+		current.next = new Node(data);
+	}
+
+	public void prepend(int data) {
+		Node newHead = new Node(data);
+		newHead.next = head;
+		head = newHead;
+	}
+
+	public void deleteWithValue(int data) {
+		if (head == null) return; //If the head is null return
+		if (head.data == data) {  //Head is equal to the next value
+			head = head.next;
+			return;
+		}
+
+		Node current = head;  //Starting at head
+		while (current.next != null) {	//Going through the linked list
+			if (current.next.data == data) { 
+				current.next = current.next.next; //Basically skip over the one you want to delete
+				return;
+			}
+			current = current.next;
+		}
+	}
+	//---------------------------------------
+	
+};
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+
 // Add a node containing "value" to position "index"
 
 void LinkedList::Insert(double value, unsigned int index) {
